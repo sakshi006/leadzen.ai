4aa7324b9c917f547a9725867722c44981707c36
Sakshi <saks5011@gmail.com>
2021-10-24 14:56:43 +0530

    feat : middle2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Middle1 from './components/Middle1/Middle1';
+import Middle2 from './components/Middle2/Middle2';
 import Navbar from './components/Navbar/navbar';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       < Navbar />
       <Header/>
       <Middle1/>
+      <Middle2/>
     </div>
   );
 }
--- src/components/Middle2/Middle2.js
+++ src/components/Middle2/Middle2.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import "./Middle2.css"
+import vid from "../../resources/we-get-what-you-mean-NLP.webm"
+import one from "../../resources/Middle2Image1.png"
+import two from "../../resources/Middle2Image2.png"
+import three from "../../resources/Middle2Image3.png"
+import four from "../../resources/Middle2Image4.png"
+
+
 
 const Middle2 = () => {
     return (
         <div>
-            
+        <div className="middle2">
+            <h2>We Get What You Mean</h2>
+            <h4>Our NLP (Natural Language Processing) based AI engine understands queries, statements and keywords to provide you with the specific datasets you are looking for.</h4>
+            </div>
+            <div className="flex-three">
+                <div className="left">
+                    <div>
+                        <img src={one} alt="one"/>
+                    </div>
+                    <div id="bottom-left">
+                        <img src={three} alt="three"/>
+                    </div>
+                </div>
+                
+                <video src={vid}type="video/mp4" autoPlay loop muted playsInline />
+
+                <div className="right">
+                   <div>
+                        <img src={two} alt="two"/>
+                    </div> 
+                    <div id="bottom-right">
+                        <img src={four} alt="four"/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
